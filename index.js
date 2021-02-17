64aef22af753a2e26b991a19beb8dbcb7449857d
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-02-17 15:56:27 +0100

    second

--- index.js
+++ index.js
@@ -14,27 +14,14 @@ let months = [
     "october",
     "november",
   ];
+  let seasons = ["winter", "spring", "summer", "autumn"];
   const curr = (monthname)=>{
-      let ses = months.indexOf(monthname);
-      if (ses == 0 || ses == 1 || ses == 2){
-          return seasons[0];
-      }
-      else if ( ses == 3 || ses == 4 || ses == 5 ){
-          return seasons[1]
-      }
-      else if(ses == 6 || ses == 7|| ses == 8){
-          return seasons[2]
-      }
-      else if(ses == 9 || ses == 10 || ses == 11){
-          return seasons|[3]
-      }
+      return seasons[Math.floor(months.indexOf(monthname) / 3)];
   }
-  let seasons = ["Winter", "Spring", "Summer", "Autumn"];
 let month = str.toLowerCase().split(" ").filter
-((word)=> months.includes(word)).join("");
-return month == "" ? `sorry`:
-`you have entred ${month} which is ${curr(month)}`
+((word) => months.includes(word)).join(" ");
+console.log(month);
+return  month = "" ? `sorry`:
+`you have entred ${month}, which is ${curr(month)}`;
 }
-console.log(monthtocheck("I love december"));
-
-
+console.log(monthtocheck("i love june"))
